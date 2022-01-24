15e90574bf190e9afe10328cc1081de5d82ff2db
norulshahlam <norulshahlam@gmail.com>
2022-01-24 22:55:54 +0800

    Unit Test

--- src/test/java/com/shah/restcrudapitutorial/controller/EmployeeControllerTest.java
+++ src/test/java/com/shah/restcrudapitutorial/controller/EmployeeControllerTest.java
@@ -1,8 +1,28 @@
 package com.shah.restcrudapitutorial.controller;
 
-import org.junit.jupiter.api.Test;
+import com.shah.restcrudapitutorial.model.entity.Employee;
+import com.shah.restcrudapitutorial.service.EmployeeService;
 
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(EmployeeController.class)
 public class EmployeeControllerTest {
+
+  
+	@Autowired
+	private MockMvc mockMvc;
+
+	@MockBean
+	private EmployeeService employeeService;
+
+	@Mock
+	private Employee employee;
+
   @Test
   void testDeleteEmployee() {
 
