bfeb957105c05410e5631248bdcf4644e7cd0fca
norulshahlam <norulshahlam@gmail.com>
2022-01-24 20:53:50 +0800

    unit-test

--- src/test/java/com/shah/restcrudapitutorial/repository/EmployeeRepositoryTest.java
+++ src/test/java/com/shah/restcrudapitutorial/repository/EmployeeRepositoryTest.java
@@ -1,9 +1,14 @@
 package com.shah.restcrudapitutorial.repository;
 
+
 import java.sql.Date;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.isA;
 
+import com.shah.restcrudapitutorial.model.CountryCount;
+import com.shah.restcrudapitutorial.model.FieldCount;
 import com.shah.restcrudapitutorial.model.entity.Employee;
 
 import org.apache.commons.lang3.RandomStringUtils;
@@ -20,15 +25,41 @@ public class EmployeeRepositoryTest {
   @Test
   public void addEmployee() {
 
-    Employee employee1 = Employee.builder().email(randomString() + "@email.com").firstName(randomString()).lastName(randomString()).gender("male").age(randomInt()).country("United States").birthDate(new Date(1987 - 03 - 29)).build();
+    Employee employee1 = Employee.builder().email(randomString() + "@email.com").firstName(randomString()).lastName(randomString()).gender("male").age(randomInt()).country("Singapore").birthDate(new Date(1987 - 03 - 29)).build();
 
-    Employee employee2 = Employee.builder().email(randomString() + "@email.com").firstName(randomString()).lastName(randomString()).gender("female").age(randomInt()).country("United States").birthDate(new Date(1987 - 03 - 29)).build();
+    Employee employee2 = Employee.builder().email(randomString() + "@email.com").firstName(randomString()).lastName(randomString()).gender("female").age(randomInt()).country("Singapore").birthDate(new Date(1987 - 03 - 29)).build();
 
-    Employee employee3 = Employee.builder().email(randomString() + "@email.com").firstName(randomString()).lastName(randomString()).gender("male").age(randomInt()).country("United States").birthDate(new Date(1987 - 03 - 29)).build();
+    Employee employee3 = Employee.builder().email(randomString() + "@email.com").firstName(randomString()).lastName(randomString()).gender("male").age(randomInt()).country("Singapore").birthDate(new Date(1987 - 03 - 29)).build();
+    
+    Employee employee4 = Employee.builder().email(randomString() + "@email.com").firstName(randomString()).lastName(randomString()).gender("male").age(randomInt()).country("Singapore").birthDate(new Date(1987 - 03 - 29)).build();
 
-    employeeRepository.saveAll(List.of(employee1, employee2, employee3));
+    employeeRepository.saveAll(List.of(employee1, employee2, employee3, employee4));
   }
 
+  
+	@Test
+	void testExistsByEmail() {
+		// given
+		// data inserted in test case above
+		// when
+		boolean expected = employeeRepository.existsByEmail("rdolling0@techcrunch.com");
+		// then
+		assertThat(expected).isTrue();
+	}
+
+  @Test
+	void testGetCountryCount() {
+		final String JAPAN = "Japan";
+		List<CountryCount> expected = employeeRepository.getCountryCount(JAPAN);
+
+		// JAPAN WILL HAVE A COUNT OF 3
+		long count = expected.stream().map(e -> e.getCount()).count();
+
+		assertThat(count).isGreaterThan(0);
+	}
+
+
+  
   private int randomInt() {
     return ThreadLocalRandom.current().nextInt(21, 55 + 1);
   }
